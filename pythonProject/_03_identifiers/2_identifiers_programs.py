11c465eb7764d848cad84276a0dc92b8b29e84c8
thokasuneel94 <suneel1994thoka@gmail.com>
2021-02-27 17:28:47 +0530

    python concepts identy and key

---
+++ pythonProject/_03_identifiers/2_identifiers_programs.py
@@ -0,0 +1,5 @@
+print("hello python")
+total=10
+print(total)
+TOTAL=20
+print(TOTAL)
---
+++ pythonProject/_04_keywords/3_keywords_programs1.py
@@ -0,0 +1,10 @@
+# if  elif   else:
+name=input("enter some name:")
+if name=="sunny":
+    print("sunny sexy")
+elif name=="kajal":
+    print("kajal figer")
+elif name=="mehrin":
+    print("mehrin beauty")
+else:
+    print("i don't no")
